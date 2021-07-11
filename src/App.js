e32a656932bfabb869c6eb019e52d0541f6dca21
Umesh Raghav <umeshraghav55@gmail.com>
2021-07-11 10:51:19 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+ 
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Header/>
+      {/*Header*/ }
+      {/*Sidebar*/ }
+      {/*React-Router*/ }
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className="header">
+                <div className="header_left">
+                    <h4>Header Left</h4>
+                </div>
+                <div className="header_center">
+                   <input type="text" />
+                </div>
+                <div className="header_right">
+                    <h4>Header Right</h4>
+                </div>
+        </div>
+    )
+}
